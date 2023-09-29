947793ace3b02a11aea77a8ac5042b5d173e8c5d
yymin1022 <yymin1022@gmail.com>
2023-09-30 01:37:14 +0900

    Update MainActivity : Added Search Button Interaction

--- app/src/main/java/com/yong/freshroute/MainActivity.kt
+++ app/src/main/java/com/yong/freshroute/MainActivity.kt
@@ -2,10 +2,22 @@ package com.yong.freshroute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btn_main_search)
+        btnSearch.setOnClickListener(btnListener)
+    }
+
+    private val btnListener = View.OnClickListener { view ->
+        when(view.id) {
+            R.id.btn_main_search -> Toast.makeText(applicationContext, "Search Button", Toast.LENGTH_LONG).show()
+        }
     }
 }
