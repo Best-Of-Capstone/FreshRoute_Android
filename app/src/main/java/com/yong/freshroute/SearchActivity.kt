abdbad705ae73d9392ba4dbf0312255730086c8f
yymin1022 <yymin1022@gmail.com>
2023-09-30 01:41:33 +0900

    Update SearchActivity : Added Detail Button Interaction

--- app/src/main/java/com/yong/freshroute/SearchActivity.kt
+++ app/src/main/java/com/yong/freshroute/SearchActivity.kt
@@ -2,10 +2,22 @@ package com.yong.freshroute
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val btnDetail = findViewById<Button>(R.id.btn_search_detail)
+        btnDetail.setOnClickListener(btnListener)
+    }
+
+    private val btnListener = View.OnClickListener { view ->
+        when(view.id) {
+            R.id.btn_search_detail -> Toast.makeText(applicationContext, "Detail Button", Toast.LENGTH_LONG).show()
+        }
     }
 }
