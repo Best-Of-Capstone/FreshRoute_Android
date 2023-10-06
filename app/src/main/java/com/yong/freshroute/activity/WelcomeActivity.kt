676d361d54e01ba5cc91411f4939f72965884100
yymin1022 <yymin1022@gmail.com>
2023-10-07 05:32:33 +0900

    Update WelcomeActivity : Applied PermissionUti;

--- app/src/main/java/com/yong/freshroute/activity/WelcomeActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/WelcomeActivity.kt
@@ -1,19 +1,15 @@
 package com.yong.freshroute.activity
 
 import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import com.yong.freshroute.R
+import com.yong.freshroute.util.PermissionUtil
+import com.yong.freshroute.util.PermissionUtil.checkLocationPermission
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var btnLocatonPermission: AppCompatButton
@@ -32,7 +28,9 @@ class WelcomeActivity : AppCompatActivity() {
     private val btnListener = View.OnClickListener { item ->
         when(item.id) {
             R.id.btn_welcome_permission_location -> {
-                requestLocationPermission.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION))
+                if(!checkLocationPermission(applicationContext)) {
+                    requestLocationPermission.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION))
+                }
             }
             R.id.btn_welcome_next -> {
                 val pref = PreferenceManager.getDefaultSharedPreferences(applicationContext)
@@ -46,18 +44,11 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private val requestLocationPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-        if(ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            || ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(applicationContext, applicationContext.getString(R.string.noti_toast_no_permission), Toast.LENGTH_LONG).show()
-            val permissionIntent = Intent().apply {
-                action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                addCategory(Intent.CATEGORY_DEFAULT)
-                data = Uri.parse("package:com.yong.freshroute")
-            }
-            applicationContext.startActivity(permissionIntent)
-        } else {
+        if(checkLocationPermission(applicationContext)) {
             btnLocatonPermission.isEnabled = false
             btnNext.isEnabled = true
+        } else {
+            PermissionUtil.openAppInfo(applicationContext)
         }
     }
 }
