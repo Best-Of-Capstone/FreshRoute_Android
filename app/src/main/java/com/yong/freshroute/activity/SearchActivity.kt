c2cbf64114e9f25119ba9a07042461ef089e4f91
yymin1022 <yymin1022@gmail.com>
2023-10-13 02:11:22 +0900

    Update SearchActivity : Added onActivityResult Intent Setup

--- app/src/main/java/com/yong/freshroute/activity/SearchActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/SearchActivity.kt
@@ -3,6 +3,7 @@ package com.yong.freshroute.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import com.yong.freshroute.R
@@ -22,6 +23,15 @@ class SearchActivity : AppCompatActivity() {
         when(view.id) {
             R.id.btn_search_detail -> startActivity(Intent(applicationContext, DetailActivity::class.java))
             R.id.btn_search_input -> startActivity(Intent(applicationContext, SearchInputActivity::class.java))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == RESULT_OK){
+            val inputStr = data!!.getStringExtra("input")
+            Log.d("Input String", inputStr!!)
         }
     }
 }
