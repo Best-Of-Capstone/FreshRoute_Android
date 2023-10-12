203b90dd738b07188967f3491c917b503fd6671b
yymin1022 <yymin1022@gmail.com>
2023-10-13 02:09:40 +0900

    Update SearchInputActivity : Added onDestroy Intent Setup

--- app/src/main/java/com/yong/freshroute/activity/SearchInputActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/SearchInputActivity.kt
@@ -1,5 +1,6 @@
 package com.yong.freshroute.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yong.freshroute.R
@@ -8,5 +9,12 @@ class SearchInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_input)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val resultIntent = Intent()
+        resultIntent.putExtra("input", "chungang-univ")
+        setResult(RESULT_OK, resultIntent)
     }
 }
