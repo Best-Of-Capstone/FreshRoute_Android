3d8d03203c184ffe34ecd05706467574ca290050
yymin1022 <yymin1022@gmail.com>
2023-10-13 21:52:34 +0900

    Update SearchActivity : Added Conditional Statements for From/To

--- app/src/main/java/com/yong/freshroute/activity/SearchActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/SearchActivity.kt
@@ -1,37 +1,53 @@
 package com.yong.freshroute.activity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.yong.freshroute.R
+import com.yong.freshroute.util.Enums
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val btnDetail = findViewById<Button>(R.id.btn_search_detail)
-        val btnInput = findViewById<Button>(R.id.btn_search_input)
+        val btnInputFrom = findViewById<Button>(R.id.btn_search_input_from)
+        val btnInputTo = findViewById<Button>(R.id.btn_search_input_to)
         btnDetail.setOnClickListener(btnListener)
-        btnInput.setOnClickListener(btnListener)
+        btnInputFrom.setOnClickListener(btnListener)
+        btnInputTo.setOnClickListener(btnListener)
+
+        activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val inputStr = result.data!!.getStringExtra("input")
+                Toast.makeText(applicationContext, inputStr, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     private val btnListener = View.OnClickListener { view ->
         when(view.id) {
             R.id.btn_search_detail -> startActivity(Intent(applicationContext, DetailActivity::class.java))
-            R.id.btn_search_input -> startActivity(Intent(applicationContext, SearchInputActivity::class.java))
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(resultCode == RESULT_OK){
-            val inputStr = data!!.getStringExtra("input")
-            Log.d("Input String", inputStr!!)
+            R.id.btn_search_input_from -> {
+                intent = Intent(applicationContext, SearchInputActivity::class.java)
+                intent.putExtra("type", Enums.SEARCH_INPUT_FROM)
+                activityResultLauncher.launch(intent)
+            }
+            R.id.btn_search_input_to -> {
+                intent = Intent(applicationContext, SearchInputActivity::class.java)
+                intent.putExtra("type", Enums.SEARCH_INPUT_TO)
+                activityResultLauncher.launch(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/yong/freshroute/activity/SearchInputActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/SearchInputActivity.kt
@@ -1,20 +1,32 @@
 package com.yong.freshroute.activity
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yong.freshroute.R
+import com.yong.freshroute.util.Enums
 
 class SearchInputActivity : AppCompatActivity() {
+    private lateinit var inputType: Enums
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_input)
+
+        if(Build.VERSION.SDK_INT >= 33){
+            inputType = intent.getSerializableExtra("type", Enums::class.java)!!
+        }else{
+            @Suppress("DEPRECATION")
+            inputType = intent.getSerializableExtra("type") as Enums
+        }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        val resultIntent = Intent()
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val resultIntent = Intent(applicationContext, SearchActivity::class.java)
+        resultIntent.putExtra("type", inputType)
         resultIntent.putExtra("input", "chungang-univ")
         setResult(RESULT_OK, resultIntent)
+        finish()
     }
 }
