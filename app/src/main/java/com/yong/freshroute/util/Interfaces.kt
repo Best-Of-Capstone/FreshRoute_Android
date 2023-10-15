2685d1c804ab18ae1f906a3f7bbc5bde269e514a
yymin1022 <yymin1022@gmail.com>
2023-10-16 04:35:22 +0900

    Update Project : Added Interface for Kakap Local API

--- app/src/main/java/com/yong/freshroute/util/Interfaces.kt
+++ app/src/main/java/com/yong/freshroute/util/Interfaces.kt
@@ -1,6 +1,11 @@
 package com.yong.freshroute.util
 
 import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
 import java.io.Serializable
 
 data class LocationData(
@@ -13,6 +18,15 @@ data class LocationData(
 enum class SearchTypes {
     SEARCH_INPUT_FROM,
     SEARCH_INPUT_TO
+}
+
+interface KakaoLocalAPI {
+    @GET("v2/local/search/keyword.JSON")
+    @FormUrlEncoded
+    fun getLocalList(
+        @Header("Authorization") apiKey: String,
+        @Field("query") queryString : String
+    ): Call<KakaoLocalList>
 }
 
 data class KakaoLocalList(
