381149d52c16d7344de0fd46f47e3c31c1b363e6
yymin1022 <yymin1022@gmail.com>
2023-10-16 04:50:17 +0900

    Update Project : Added Retrofit Util

---
+++ app/src/main/java/com/yong/freshroute/util/RetrofitUtil.kt
@@ -0,0 +1,26 @@
+package com.yong.freshroute.util
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object KakaoLocalClient {
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .build()
+
+    private val gsonBuilder = GsonBuilder().setLenient().create()
+
+    private val retrofitClient = Retrofit.Builder()
+        .baseUrl("https://dapi.kakao.com/")
+        .addConverterFactory(GsonConverterFactory.create(gsonBuilder))
+        .client(okHttpClient)
+        .build()
+
+    val kakaoLocalService: KakaoLocalAPI by lazy {
+        retrofitClient.create(KakaoLocalAPI::class.java)
+    } }
