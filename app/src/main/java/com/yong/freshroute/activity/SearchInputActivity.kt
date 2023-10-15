e14cc6bbc20fb317e96ac07d9a7e4bb64c091188
yymin1022 <yymin1022@gmail.com>
2023-10-16 04:08:15 +0900

    Update SearchInputActivity : Applied SearchData Data Class

--- app/src/main/java/com/yong/freshroute/activity/SearchInputActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/SearchInputActivity.kt
@@ -4,16 +4,28 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.addCallback
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.yong.freshroute.R
 import com.yong.freshroute.util.Enums
+import com.yong.freshroute.util.SearchData
 
 class SearchInputActivity : AppCompatActivity() {
+    private lateinit var btnSearch: MaterialButton
+    private lateinit var edSearchKeyword: TextInputEditText
+
+    private lateinit var inputData: SearchData
     private lateinit var inputType: Enums
-    private var inputData = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_input)
+
+        btnSearch = findViewById(R.id.btn_searchinput_search)
+        edSearchKeyword = findViewById(R.id.et_searchinput_text)
+        btnSearch.setOnClickListener(btnListener)
 
         if(Build.VERSION.SDK_INT >= 33){
             inputType = intent.getSerializableExtra("type", Enums::class.java)!!
@@ -23,15 +35,28 @@ class SearchInputActivity : AppCompatActivity() {
         }
 
         this.onBackPressedDispatcher.addCallback(this) {
-            val resultIntent = Intent(applicationContext, SearchActivity::class.java)
-            resultIntent.putExtra("type", inputType)
-            if(inputData.isNotEmpty()){
-                resultIntent.putExtra("input", "chungang-univ")
-                setResult(RESULT_OK, resultIntent)
-            }else{
-                setResult(RESULT_CANCELED, resultIntent)
-            }
-            finish()
+            onBackListener()
         }
+    }
+
+    private val btnListener = View.OnClickListener { view ->
+        when(view.id) {
+            R.id.btn_searchinput_search -> {
+                inputData = SearchData("Name", "Address", 1.0, 1.0)
+
+                val resultIntent = Intent(applicationContext, SearchActivity::class.java)
+                resultIntent.putExtra("data", inputData)
+                resultIntent.putExtra("type", inputType)
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+    }
+
+    private fun onBackListener() {
+        val resultIntent = Intent(applicationContext, SearchActivity::class.java)
+        resultIntent.putExtra("type", inputType)
+        setResult(RESULT_CANCELED, resultIntent)
+        finish()
     }
 }
