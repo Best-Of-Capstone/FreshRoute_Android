de31c634fdac2c83419b38715164558dfae63577
yymin1022 <yymin1022@gmail.com>
2023-11-01 00:59:03 +0900

    Update Project : Added Firebase Auth Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,10 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-auth-ktx")
 
     implementation("com.kakao.maps.open:android:2.6.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
