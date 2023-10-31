44283d2895b9d4912555f32f84119b9cca03a27c
yymin1022 <yymin1022@gmail.com>
2023-11-01 00:42:54 +0900

    Update AuthUtil : Added Dummy Functions for Firebase Auth

--- app/src/main/java/com/yong/freshroute/util/AuthUtil.kt
+++ app/src/main/java/com/yong/freshroute/util/AuthUtil.kt
@@ -1,4 +1,19 @@
 package com.yong.freshroute.util
 
 object AuthUtil {
+    fun isNewUser(userID: String) {
+
+    }
+
+    fun isTokenAvail(userToken: String) {
+
+    }
+
+    fun tryFirebaseLogin() {
+        
+    }
+
+    fun tryLogin(userToken: String?) {
+
+    }
 }
