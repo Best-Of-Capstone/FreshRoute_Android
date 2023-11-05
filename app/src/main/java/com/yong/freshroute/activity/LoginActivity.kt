6b903059996293f03e53d1661f7158316fd98498
yymin1022 <yymin1022@gmail.com>
2023-11-06 03:25:40 +0900

    Update LoginActivity : Added Button Logic

--- app/src/main/java/com/yong/freshroute/activity/LoginActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/LoginActivity.kt
@@ -2,11 +2,24 @@ package com.yong.freshroute.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.button.MaterialButton
 import com.yong.freshroute.R
+import com.yong.freshroute.util.AuthUtil
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var btnGoogleLogin: MaterialButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnGoogleLogin = findViewById(R.id.btn_login_google)
+        btnGoogleLogin.setOnClickListener(btnListener)
+    }
+
+    private val btnListener = View.OnClickListener { view ->
+        when(view.id) {
+            R.id.btn_login_google -> AuthUtil.tryLogin()
+        }
     }
 }
