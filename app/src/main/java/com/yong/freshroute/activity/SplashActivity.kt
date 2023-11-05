5b758261e5809c3602d7fd46d91a9394131447c5
yymin1022 <yymin1022@gmail.com>
2023-11-06 03:00:14 +0900

    Update SplashActivity : Added Splash Logic

--- app/src/main/java/com/yong/freshroute/activity/SplashActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.yong.freshroute.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.yong.freshroute.R
+import com.yong.freshroute.util.AuthUtil
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            if(AuthUtil.isLoggedIn() && AuthUtil.isTokenAvail()){
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        }, 2000)
     }
 }
