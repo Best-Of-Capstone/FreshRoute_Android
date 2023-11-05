985ee7ca0869b5300ba7242f3ae465f251e351bf
yymin1022 <yymin1022@gmail.com>
2023-11-06 04:01:04 +0900

    Update LoginActivity : Cleaned UP

--- app/src/main/java/com/yong/freshroute/activity/LoginActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
