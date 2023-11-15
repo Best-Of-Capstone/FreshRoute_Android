abea75446bda424d6265d92a948c7f9d9e2df076
yymin1022 <yymin1022@gmail.com>
2023-11-15 15:51:18 +0900

    Update Project : Added SearchResult Activity

---
+++ app/src/main/java/com/yong/freshroute/activity/SearchResultActivity.kt
@@ -0,0 +1,12 @@
+package com.yong.freshroute.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yong.freshroute.R
+
+class SearchResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result)
+    }
+}
