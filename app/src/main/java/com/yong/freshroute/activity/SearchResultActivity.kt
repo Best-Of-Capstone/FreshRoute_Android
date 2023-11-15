9827ab093283cd8e98b2e782f3c90edf1cbae09a
yymin1022 <yymin1022@gmail.com>
2023-11-15 16:23:24 +0900

    Update SearchResultAvtivity : Applied AppBar as ToolBar

--- app/src/main/java/com/yong/freshroute/activity/SearchResultActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/SearchResultActivity.kt
@@ -2,11 +2,17 @@ package com.yong.freshroute.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.yong.freshroute.R
 
 class SearchResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_result)
+
+        val toolbar = findViewById<Toolbar>(R.id.searchresult_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = getString(R.string.searchresult_toolbar_title)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 }
