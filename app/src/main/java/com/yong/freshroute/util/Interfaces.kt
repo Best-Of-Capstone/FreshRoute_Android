8c234dc7c8caa5d7e857db9489c316cc8430f6b5
yymin1022 <yymin1022@gmail.com>
2023-11-21 10:33:03 +0900

    Update Interfaces : Added Retrofit Interfaces for Route API

--- app/src/main/java/com/yong/freshroute/util/Interfaces.kt
+++ app/src/main/java/com/yong/freshroute/util/Interfaces.kt
@@ -2,8 +2,10 @@ package com.yong.freshroute.util
 
 import com.google.gson.annotations.SerializedName
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 import retrofit2.http.Query
 import java.io.Serializable
 
@@ -43,3 +45,45 @@ data class KakaoLocalItem(
     @SerializedName("x") val localLongitude: String,
     @SerializedName("y") val localLatitude: String
 )
+
+data class ApiResult<T>(
+    val RESULT_CODE: Int,
+    val RESUMT_MSG: String,
+    val RESULT_DATA: T
+)
+
+data class RouteApiInput(
+    val startCord: String,
+    val endCord: String,
+    val targetCount: Int?
+)
+
+data class RouteApiResultItemDataStep(
+    val distance: Int,
+    val duration: Int,
+    val type: String,
+    val name: String,
+    val wayPoints: List<Int>
+)
+
+data class RouteApiResultItemData(
+    val distance: String,
+    val duration: String,
+    val steps: Array<RouteApiResultItemDataStep>
+)
+
+data class RouteApiResultItem(
+    val id: Int,
+    val description: String,
+    val route: RouteApiResultItemData
+)
+
+data class RouteApiResult(
+    val routeList: Array<RouteApiResultItem>,
+
+)
+
+interface RouteApi {
+    @POST("findRouter")
+    fun getRouteList(@Body coordData: RouteApiInput): Call<ApiResult<RouteApiResult>>
+}
