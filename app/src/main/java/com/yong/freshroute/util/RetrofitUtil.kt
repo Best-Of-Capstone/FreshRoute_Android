2f1ff8f085a9335093d1882e7c55eaf6976d662f
yymin1022 <yymin1022@gmail.com>
2023-11-21 10:36:55 +0900

    Update Retrofit Util : Added Retrofit Object for Route API

--- app/src/main/java/com/yong/freshroute/util/RetrofitUtil.kt
+++ app/src/main/java/com/yong/freshroute/util/RetrofitUtil.kt
@@ -25,3 +25,23 @@ object KakaoLocalClient {
         retrofitClient.create(KakaoLocalAPI::class.java)
     }
 }
+
+object RouteApiClient {
+    private val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .readTimeout(120, TimeUnit.SECONDS)
+        .writeTimeout(120, TimeUnit.SECONDS)
+        .build()
+
+    private val gsonBuilder = GsonBuilder().setLenient().create()
+
+    private val retrofitClient = Retrofit.Builder()
+        .baseUrl("https://freshroute.dev-lr.com/")
+        .addConverterFactory(GsonConverterFactory.create(gsonBuilder))
+        .client(okHttpClient)
+        .build()
+
+    val RouteApiService: RouteApi by lazy {
+        retrofitClient.create(RouteApi::class.java)
+    }
+}
