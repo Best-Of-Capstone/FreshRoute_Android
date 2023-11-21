6ab6a0d737496a5fe5b1c22042d60f3263b79dac
yymin1022 <yymin1022@gmail.com>
2023-11-22 00:00:28 +0900

    Update DetailActivity : Added Logic for Get Route Data from Intent

--- app/src/main/java/com/yong/freshroute/activity/DetailActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/DetailActivity.kt
@@ -1,12 +1,26 @@
 package com.yong.freshroute.activity
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.yong.freshroute.R
+import com.yong.freshroute.util.RouteApiResultItem
 
 class DetailActivity : AppCompatActivity() {
+    var routeData: RouteApiResultItem? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        if(Build.VERSION.SDK_INT >= 33){
+            routeData = intent.getSerializableExtra("route_data", RouteApiResultItem::class.java)!!
+        }else{
+            @Suppress("DEPRECATION")
+            routeData = intent.getSerializableExtra("from") as RouteApiResultItem
+        }
+
+        Toast.makeText(applicationContext, routeData.toString(), Toast.LENGTH_LONG).show()
     }
 }
