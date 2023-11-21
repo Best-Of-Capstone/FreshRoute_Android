3869eb56b70ee3a7095b95fce0113607d9ab377b
yymin1022 <yymin1022@gmail.com>
2023-11-22 02:44:58 +0900

    Update DetailActivity : Seperated Camera Moving Function

--- app/src/main/java/com/yong/freshroute/activity/DetailActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/DetailActivity.kt
@@ -1,6 +1,5 @@
 package com.yong.freshroute.activity
 
-import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -21,7 +20,6 @@ import com.kakao.vectormap.route.RouteLineStyles
 import com.kakao.vectormap.route.RouteLineStylesSet
 import com.yong.freshroute.R
 import com.yong.freshroute.adapter.RouteDetailRecyclerAdapter
-import com.yong.freshroute.adapter.RouteListRecyclerAdapter
 import com.yong.freshroute.util.RouteApiResultItem
 
 class DetailActivity : AppCompatActivity() {
@@ -65,12 +63,8 @@ class DetailActivity : AppCompatActivity() {
             override fun onMapError(error: Exception) {}
         }, object : KakaoMapReadyCallback() {
             override fun onMapReady(kakaoMap: KakaoMap) {
-                if(routeData != null){
-                    val cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(routeData!!.route.coordinates[0][0].toDouble(), routeData!!.route.coordinates[0][1].toDouble()))
-                    kakaoMap.moveCamera(cameraUpdate)
-
-                    drawRoute(kakaoMap)
-                }
+                drawRoute(kakaoMap)
+                moveCamera(kakaoMap, routeData!!.route.coordinates[0][0], routeData!!.route.coordinates[0][1])
             }
         })
     }
@@ -87,5 +81,10 @@ class DetailActivity : AppCompatActivity() {
 
         val routeOptions = RouteLineOptions.from(routeSegment).setStylesSet(routeStyle)
         routeLayer.addRouteLine(routeOptions)
+    }
+
+    private fun moveCamera(kakaoMap: KakaoMap, lat: Number, long: Number) {
+        val cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(lat.toDouble(), long.toDouble()))
+        kakaoMap.moveCamera(cameraUpdate)
     }
 }
