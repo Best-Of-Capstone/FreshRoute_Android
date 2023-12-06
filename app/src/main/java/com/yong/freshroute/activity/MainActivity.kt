c7eccc8427bd4379ce9fe05432c480bf7a32d8ee
yymin1022 <yymin1022@gmail.com>
2023-12-07 03:09:42 +0900

    Update MainActivity : Applied Improved Layout Elements

--- app/src/main/java/com/yong/freshroute/activity/MainActivity.kt
+++ app/src/main/java/com/yong/freshroute/activity/MainActivity.kt
@@ -7,13 +7,9 @@ import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.kakao.vectormap.KakaoMap
-import com.kakao.vectormap.KakaoMapReadyCallback
-import com.kakao.vectormap.LatLng
-import com.kakao.vectormap.MapLifeCycleCallback
-import com.kakao.vectormap.camera.CameraUpdateFactory
 import com.yong.freshroute.R
 import com.yong.freshroute.util.AuthUtil
 import com.yong.freshroute.util.PermissionUtil.checkLocationPermission
@@ -22,8 +18,12 @@ import com.yong.freshroute.util.PermissionUtil.openAppInfo
 class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
+    private lateinit var mainAnimWeather: LottieAnimationView
     private lateinit var mainBtnSearch: LinearLayout
-    private lateinit var mainWeatherText: TextView
+    private lateinit var mainGreetText: TextView
+    private lateinit var mainWeatherDescText: TextView
+    private lateinit var mainWeatherInfoText: TextView
+    private lateinit var mainWeatherTempText: TextView
     private lateinit var mainWelcomeText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,11 +33,15 @@ class MainActivity : AppCompatActivity() {
         mainBtnSearch = findViewById(R.id.btn_main_search)
         mainBtnSearch.setOnClickListener(btnListener)
 
-        mainWeatherText = findViewById(R.id.tv_main_weather)
+        mainAnimWeather = findViewById(R.id.anim_main_weather)
+        mainGreetText = findViewById(R.id.tv_main_greeting)
+        mainWeatherDescText = findViewById(R.id.tv_main_weather_desc)
+        mainWeatherInfoText = findViewById(R.id.tv_main_weather_info)
+        mainWeatherTempText = findViewById(R.id.tv_main_weather_temp)
         mainWelcomeText = findViewById(R.id.tv_main_welcome)
 
-        mainWeatherText.text = getString(R.string.main_tv_weather_default)
-        mainWelcomeText.text = String.format(getString(R.string.main_tv_welcome_format), AuthUtil.getUserInfo()!!.Name)
+        mainWeatherInfoText.text = getString(R.string.main_tv_weather_default)
+        mainGreetText.text = String.format(getString(R.string.main_tv_welcome_format), AuthUtil.getUserInfo()!!.Name)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(applicationContext)
     }
@@ -49,7 +53,15 @@ class MainActivity : AppCompatActivity() {
             openAppInfo(applicationContext)
         } else {
             // Update Weather
+            updateWeatherView("Today will be sunny", 25, "SUNNY")
         }
+    }
+
+    private fun updateWeatherView(desc: String, temp: Int, type: String) {
+        mainAnimWeather.setAnimation(R.raw.anim_weather_sunny)
+        mainWeatherDescText.text = desc
+        mainWeatherInfoText.text = type
+        mainWeatherTempText.text = temp.toString()
     }
 
     private val btnListener = View.OnClickListener { view ->
